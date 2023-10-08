e2fbeb60ad0c46c1305cae7535177b9666528825
rozhandad <145255916+rozhandad@users.noreply.github.com>
2023-10-08 15:55:23 +0330

    Add files via upload

---
+++ Quiz/html/js/timer.js
@@ -0,0 +1,40 @@
+function startTimer() {
+    let s = 0;
+    let m = 0;
+    let h = 0;
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+    let timer = document.getElementById("timer");
+
+    quizTimer = setInterval(function() {
+      s++;
+
+      if (s > 59) {
+        s = 0;
+        m++;
+        seconds = "0" + s;
+      }
+
+      if (m > 59) {
+        m = 0;
+        h++;
+        minutes = "00";
+      }
+
+      seconds = s > 9 ? s : "0" + s;
+      minutes = m > 9 ? m : "0" + m;
+      hours = h > 9 ? h : "0" + h;
+
+      timeSpent = h
+        ? hours + ":" + minutes + ":" + seconds
+        : minutes + ":" + seconds;
+      timer.textContent = timeSpent;
+    }, 1000);
+  }
+function gettime(){
+var s;
+s=document.getElementById("timer").innerHTML;
+ document.getElementById("usertime").innerHTML=s;
+document.getElementById("timer").style.display="none";
+}
